2d0571729438db06dc0b918f12ba9bb4671cc203
sh060104 <shreyaraut2004@gmail.com>
2024-09-01 21:35:04 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import player.Player;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Player p=new Player("shreya",8);
+        System.out.println(p.getPlayername());
+
+    }
+}
